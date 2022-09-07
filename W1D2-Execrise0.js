23cd852da1b751ffb7084fce18661a91d7d2c189
ricksrocket <112906119+ricksrocket@users.noreply.github.com>
2022-09-07 09:01:04 -0500

    Create W1D2-Execrise0.js

---
+++ W1D2-Execrise0.js
@@ -0,0 +1,23 @@
+/*
+1. Write a function, that computes the area of a triangle using Heron's formula
+    Area= sqrt{ s(s-a)(s-b)(s-c) }
+    where s = (a+b+c)/s (s is known as the semi-perimeter)
+    where a,b,c are the lengths of the three triangle sides
+
+    write a separate function to compute the semi-perimeter
+
+*/
+function computeArea (sideA, sideB, sideC) {
+    let s;
+    function semiPerimeter (sideA,sideB,sideC) {
+        s= (sideA+sideB+sideC)/2;
+        return s;
+    }
+    semiPerimeter (sideA, sideB, sideC);
+    area = Math.sqrt(s*(s-sideA)*(s-sideB)*(s-sideC));
+
+return area;
+
+}
+
+console.log((computeArea(3,4,5)));
