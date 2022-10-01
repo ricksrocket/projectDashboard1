c8dea0dcf07c753182d5c00882510f26fde91dfe
ricksrocket <112906119+ricksrocket@users.noreply.github.com>
2022-10-01 08:36:48 -0500

    Update bank.js

--- Final Project/bank.js
+++ Final Project/bank.js
@@ -4,13 +4,14 @@
 /* global exports require Account   SavingsAccount CheckingAccount */
 
 /* exports at end of file since exporting an object, which can only be referenced after definition   */
-
+/*
 const acc = require("./account.js");//with node need the name of your file with your functions here
 const Account = acc.Account;  //do this for all of the functions used in the Mocha tests
 const chk = require("./checkingaccount.js");
 const CheckingAccount = chk.CheckingAccount;
 const sav = require("./savingsaccount.js");
 const SavingsAccount = sav.SavingsAccount;
+*/
 
 
 
@@ -19,15 +20,19 @@ class Bank {
         this._accounts = [];
     }
     addAccount() {
+        this._accounts.push(_number);
+        return this._number
         
     }
 
     addSavingsAccount(interest) {
+        return this._number
 
     }
 
 
     addCheckingAccount(overdraft) {
+        return this._number
 
     }
 
